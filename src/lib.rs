a8693a0adbc7ef222b64aee50bb4f954af1a9049
Peter <38674653+Monology@users.noreply.github.com>
2021-07-12 02:59:33 -0400

    remove broken test

--- src/lib.rs
+++ src/lib.rs
@@ -31,35 +31,3 @@ pub fn reduce_colors<T: GenericImage> (image: T, colors: &[Rgb<u8>]) -> RgbImage
 
     new_image
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::reduce_colors;
-    use image::Rgb;
-
-    #[test]
-    fn it_works() {
-        let image = image::open("old-image.jpg").unwrap();
-        let new_image = reduce_colors(image, &[
-            Rgb::from([240,207,149]),
-            Rgb::from([24,30,75]),
-            Rgb::from([10,6,11]),
-            Rgb::from([142,117,98]),
-            Rgb::from([67,62,70]),
-            Rgb::from([28,42,84]),
-            Rgb::from([19,13,20]),
-            Rgb::from([248,217,158]),
-            Rgb::from([9,4,7]),
-            Rgb::from([62,53,52]),
-            Rgb::from([62,71,99]),
-            Rgb::from([134,125,118]),
-            Rgb::from([198,154,106]),
-            Rgb::from([15,14,59]),
-            Rgb::from([113,90,78]),
-            Rgb::from([255, 255, 255]),
-            Rgb::from([255, 249, 189]),
-        ]);
-
-        new_image.save("new.jpg");
-    }
-}
